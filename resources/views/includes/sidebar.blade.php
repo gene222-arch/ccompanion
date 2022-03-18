bf37b53ac78a938868ec759c120ac7e2beb8ae23
gene222-arch <genephillip222@gmail.com>
2022-03-18 13:59:44 +0700

    feat: add dashboard layout

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,44 @@
+<nav id="sidebar" class="sidebar-container">
+    <div class="custom-menu">
+      <button type="button" id="sidebarCollapse" class="btn btn-primary">
+        <i class="fa fa-bars"></i>
+        <span class="sr-only">Toggle Menu</span>
+    </button>
+  </div>
+	<div class="p-3">
+		<div class="row mb-5 mt-3 justify-content-center align-items-center">
+			<div class="col-3">
+				<img class="img img-responsive" src="{{ asset("storage/app/logo.png") }}" width="40" height="40">
+			</div>
+			<div class="col">
+				<h6><strong>{{ Auth::user()->name }}</strong></h6>
+				<small>{{ Auth::user()->email }}</small>
+			</div>
+		</div>
+      <ul class="list-unstyled components mb-5">
+			<li class="{{ request()->is('/') ? 'active' : '' }}">
+				<a href="/"><span class="fa fa-home mr-3 {{ request()->is('/') ? 'text-info' : '' }}"></span> Dashboard</a>
+			</li>
+			@hasrole('Administrator')
+				<li class="{{ request()->is('residents') || request()->is('residents/*') ? 'active' : '' }}">
+					<a href="/residents"><i class="fas fa-user mr-3 {{ request()->is('residents') || request()->is('residents/*') ? 'text-info' : '' }}"></i> Residents</a>
+				</li>
+			@endhasrole
+			<li>
+				<a href="#" class="nav-link" onclick="document.getElementById('logout__form').submit()">
+					<p><i class="fas fa-sign-out-alt mr-3 text-light"></i>Logout</p>
+					<form action="{{ route('logout') }}" method="POST" id="logout__form">
+						@csrf
+					</form>
+              	</a>
+			</li>
+      </ul>
+
+      <div class="footer">
+		<p>Copyright &copy;
+			<script>document.write(new Date().getFullYear());</script> All rights reserved
+		</p>
+      </div>
+
+  </div>
+</nav>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>@yield('title')</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <script src="https://kit.fontawesome.com/47fd6b8a56.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  @yield('links')
+  @yield('css')
+</head>
+
+<body>
+    <div class="wrapper d-flex align-items-stretch">
+        @include('includes.sidebar')
+        <div id="content" class="p-4 p-md-5 pt-5">
+            @include('includes.navbar')
+            @yield('content')
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/navbar.js') }}"></script>
+    @yield('js')
+</body>
+</html>
