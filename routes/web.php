bc98366a6995ae23d3257ee7f393bb778581b7a0
gene222-arch <genephillip222@gmail.com>
2022-03-18 18:20:03 +0700

    feat: add index resource for department

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware('auth')->group(function ()
 {
     Route::resource('courses', CourseController::class);
+    Route::resource('departments', DepartmentController::class);
 });
