1e230ac2107483c89323f071d1b44dc6f9aa15eb
gene222-arch <genephillip222@gmail.com>
2022-03-20 17:15:10 +0800

    feat: implement soft delete to Student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,10 +6,12 @@ use App\Models\User;
 use App\Models\Department;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
