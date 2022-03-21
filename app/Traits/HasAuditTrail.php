df8d5c0b437327c29974299953c93eef2c52ad2b
gene222-arch <genephillip222@gmail.com>
2022-03-21 17:58:35 +0800

    feat: implement crud event in bootable method

--- app/Traits/HasAuditTrail.php
+++ app/Traits/HasAuditTrail.php
@@ -9,4 +9,27 @@ Trait HasAuditTrail
     {
         return $this->morphMany(AuditTrail::class, 'audit_trailable');
     }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::created(function ($model) {
+            $this->auditTrails()->create([
+                'action' => 'Create'
+            ]);
+        });
+
+        self::updated(function ($model) {
+            $this->auditTrails()->create([
+                'action' => 'Update'
+            ]);
+        });
+
+        self::deleted(function ($model) {
+            $this->auditTrails()->create([
+                'action' => 'Delete'
+            ]);
+        });
+    }
 }
