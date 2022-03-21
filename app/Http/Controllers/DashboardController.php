6561100245ab68fcbfbb7933f93e9a53278f4425
gene222-arch <genephillip222@gmail.com>
2022-03-21 18:13:48 +0800

    refactor: change file name and remove middleware

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,20 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Course;
-use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class DashboardController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AnnouncementController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\PageController;
-use App\Http\Controllers\ProfessorController;
-use App\Http\Controllers\RegistrarController;
-use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfessorController;
+use App\Http\Controllers\RegistrarController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\AnnouncementController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,7 +33,7 @@ Auth::routes([
 
 Route::middleware('auth')->group(function ()
 {
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('announcements', AnnouncementController::class);
     Route::get('/enable/{announcement}', [AnnouncementController::class, 'toggleEnable'])->name('announcements.enabled');
     Route::resource('administrators', AdminController::class);
